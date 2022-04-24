1b4112c9c591c2d9224128d39bb22eed23d77f9e
williamnoonan <willnoonan@outlook.com>
2022-04-24 12:26:35 -0500

    view function to render map template

--- map/views.py
+++ map/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
+
+
+def map_hello_world(request):
+    """
+    Renders a page with embedded Google map. Passes variables to the associated html template via dictionary
+    'context'. The URL associated with this view function is defined in urls.py.
+    """
+
+    context = {
+        "google_api_key": settings.GOOGLE_API_KEY,
+        "lat_coord": 29.4190,
+        "lng_coord": -98.4836,
+    }
+    return render(request, 'map/map.html', context)
