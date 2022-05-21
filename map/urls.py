513ee74de6045b2e0d4f9fbe776bcfa29449410a
williamnoonan <willnoonan@outlook.com>
2022-05-21 13:17:21 -0500

    reformat for new urls

--- map/urls.py
+++ map/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 app_name = "map"
 urlpatterns = [
+    # Map:
     path('map/', views.map_hello_world, name='map'),
 
+    # Socket Test:
     path('socket-test/', views.socket_view, name='socket-page'),
+
+    # AJAX Test:
     path('ajax-test/', views.ajax_view, name='ajax-page'),
     path('ajax-hidden/', views.ajax_background_request_handler, name='ajax-hidden'),  # not seen by user, for
     # background processing use only
